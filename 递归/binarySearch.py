cd3911ea45e57e77ea1d41f1a3bcdf27b943bea8
vicent <603413691@qq.com>
2021-04-26 15:48:25 +0800

    change

---
+++ 递归/binarySearch.py
@@ -0,0 +1,16 @@
+def binarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist) // 2
+        if alist[midpoint] == item:
+            return True
+        else:
+            if item < alist[midpoint]:
+                return binarySearch(alist[:midpoint], item)
+            else:
+                return binarySearch(alist[midpoint+1:], item)
+
+alist = [1,2,3,4,5,6,7,8,9]
+flag = binarySearch(alist, 10)
+print(flag)
