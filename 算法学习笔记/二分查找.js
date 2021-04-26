890cdd05c179d9bd3dc4988d2e44181653c58218
vicent <603413691@qq.com>
2021-04-26 15:38:51 +0800

    change binarySearch

--- 算法学习笔记/二分查找.js
+++ 算法学习笔记/二分查找.js
@@ -1,27 +1,22 @@
+// 对顺序表的二分查找
 Array.prototype.binarySearch = function (target) {
-    let len = this.length,
-    mid = Math.floor(len/2),
-    leftmark = 0,
-    rightmark = len,
-    result = false;
-    while (!result) {
-        if (target > this[mid]) {
-            leftmark = mid;
-            mid = leftmark +  Math.floor((rightmark - leftmark)/2);
+    let leftmark = 0,
+    rightmark = this.length - 1,
+    found = false;
+    while (leftmark <= rightmark && !found) {
+        let mid = Math.floor((rightmark+leftmark)/2);
+        if (this[mid] == target) {
+            found = true;
         } else if (target < this[mid]) {
-            rightmark = mid;
-            mid = leftmark + Math.floor((rightmark - leftmark)/2);
-        } else if (target == this[mid]){
-            result = true;
-        }
-        if (leftmark >= rightmark) {
-            return -1;
+            rightmark = mid - 1;
+        } else if (target > this[mid]) {
+            leftmark = mid + 1;
         }
     }
-    return result;
+    return found;
 }
 
 
 const arr = [1,2,3,4,5,6,7,8,9];
-let flag = arr.binarySearch(10);
+let flag = arr.binarySearch(3);
 console.log(flag)
